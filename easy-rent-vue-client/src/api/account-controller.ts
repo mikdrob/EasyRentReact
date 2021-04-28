9d3838d34dd35e6e15aa5d87a793657f9d79a1d0
midrob <midrob@ttu.ee>
2021-04-28 15:37:22 +0300

    vue rebase

--- easy-rent-vue-client/src/api/account-controller.ts
+++ easy-rent-vue-client/src/api/account-controller.ts
@@ -1,8 +1,19 @@
 import { ILoginInfo } from "@/store";
+import axios, { AxiosResponse } from "axios";
 
 export default class AccountController {
-    constructor(protected endPointUrl: string){
-
+    private url: string;
+    constructor(endPointUrl: string) {
+        this.url = endPointUrl;
     }
-    
+
+    async logIn(login: ILoginInfo): Promise<AxiosResponse> {
+        const loginDataStr = JSON.stringify(login);
+        const response = await axios.post(
+            this.url,
+            loginDataStr,
+            { headers: { 'Content-type': 'application/json' } }
+        );
+        return response;
+    }
 }
